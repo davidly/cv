b31344d5144d2b36c5a11ae51ed590147529e263
David Lee <davidly_lee@hotmail.com>
2022-02-06 18:27:24 -0800

    Update m.bat

--- m.bat
+++ m.bat
@@ -1,6 +1,6 @@
 @echo off
 del cv.exe
 del cv.pdb
-cl /nologo cv.cxx /O2it /EHac /Zi /Gy /D_AMD64_ /link ntdll.lib /OPT:REF
+cl /nologo cv.cxx /I.\ /O2it /EHac /Zi /Gy /D_AMD64_ /link ntdll.lib /OPT:REF
 
 
